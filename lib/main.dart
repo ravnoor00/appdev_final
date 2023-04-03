1b3f87411ae38ca8f8a6b05d0f7fd953da0b8fa6
bedirbxsci <bedir@bxscience.edu>
2023-04-03 09:14:02 -0400

    mane

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:makequiz/screens/home.dart';
+import 'firebase_options.dart';
 
 
-void main() async {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
