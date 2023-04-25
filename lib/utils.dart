fc9a4111a2aa3ae250dc8fdeb913b78efffff9c7
Jeffrey Li <jeffreyli8000@gmail.com>
2023-04-25 09:30:49 -0400

    lol

--- lib/utils.dart
+++ lib/utils.dart
@@ -5,3 +5,9 @@ String appName = "Study App";
 Color yellow = const Color(0xffFFFCF2);
 Color redorange = const Color(0xffEB5E27);
 Color textField = const Color(0xffF2EDDF);
+
+void navigate(BuildContext context, Widget w) {
+  Navigator.of(context).push(
+    MaterialPageRoute(builder: (context) => w),
+  );
+}
